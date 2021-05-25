f3b5be3e1c7200e77bb3212f32e6d22e18b94e5e
Andy Baran <andy.baran@hashicorp.com>
2021-05-25 06:02:51 -0400

    build.sh changes

--- terragpio/build.sh
+++ terragpio/build.sh
@@ -1,9 +1,27 @@
 #! /bin/sh
 
-cd cmd/client
-GOOS=linux GOARCH=arm go build -v ./...
-cd ../server
-GOOS=linux GOARCH=arm go build -v ./...
+# cd cmd/client
+# GOOS=linux GOARCH=arm go build -v ./...
+# cd ../server
+echo "************************************************"
+echo "Building"
+echo "************************************************"
+echo ""
+GOOS=linux GOARCH=arm go build -v -a ./...
+echo ""
+echo "************************************************"
+echo "Done building"
+echo "************************************************"
+echo ""
+echo "************************************************"
+echo "Copying"
+echo "************************************************"
+echo ""
 cd ../..
 scp ./cmd/server/server pi@10.15.21.124:/home/pi/bins
 scp ./cmd/client/client pi@10.15.21.124:/home/pi/bins
+echo ""
+echo "************************************************"
+echo "All done"
+echo "************************************************"
+echo ""
