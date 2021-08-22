009dbed91b05b8f39ba0b46540fb8ad0695166e0
Andy Baran <andy@andybaran.net>
2021-08-22 13:36:08 -0400

    Bme280 (#3)
    * can now read i2c bme280 device
    
    * BME280 implemented
    
    * added setbme280

--- terragpio/gpioclient/gpioclient.go
+++ terragpio/gpioclient/gpioclient.go
@@ -21,12 +21,31 @@ type SetPWMArgs struct {
 	Freq      string
 }
 
+type SetBME280Args struct {
+	I2CBus  string
+	I2CAddr uint64
+}
+
 func (c *Client) SetPWM(args SetPWMArgs) error {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	actualsettings, err := c.c.SetPWM(ctx, &pb.PWMRequest{Pin: args.Pin, Dutycycle: args.DutyCycle, Frequency: args.Freq})
+	if err != nil {
+		log.Fatalf("Error : ", c, err)
+		return err
+	}
+	fmt.Println(actualsettings)
+	return nil
+}
+
+func (c *Client) SetBME280(args SetBME280Args) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	actualsettings, err := c.c.SetBME280(ctx, &pb.BME280Request{I2Cbus: args.I2CBus, I2Caddr: args.I2CAddr})
 	if err != nil {
 		log.Fatalf("Error : ", c, err)
 		return err
