690a7dba9ed21cea3b072a8b03484c39823518fb
Andy Baran <andy.baran@hashicorp.com>
2021-11-29 19:13:57 -0500

    building at home again

--- terragpio/build.sh
+++ terragpio/build.sh
@@ -4,8 +4,6 @@ echo "Building"
 GOOS=linux GOARCH=arm go build -o ./server ./cmd/server/rpiserver.go 
 GOOS=linux GOARCH=arm go build -o ./client ./cmd/client/rpiclient.go    
 echo "Copying"
-scp ./server pi@10.0.0.157:/home/pi/bins
-#scp ./server pi@10.15.21.124:/home/pi/bins
+scp ./server pi@10.15.21.124:/home/pi/bins
 scp ./client pi@10.0.0.157:/home/pi/bins
-#scp ./client pi@10.15.21.124:/home/pi/bins
 echo "***********     All Done        ***********"
