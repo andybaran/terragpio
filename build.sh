946e5241b4aeef1451cdbdebf359ca6cad0e550d
Andy Baran <andy.baran@hashicorp.com>
2023-03-06 16:31:50 -0500

    bad habits

--- build.sh
+++ build.sh
@@ -4,14 +4,9 @@ echo "Building"
 GOOS=linux GOARCH=arm go build -o ./server ./cmd/server/terragpioserver.go  
 GOOS=linux GOARCH=arm go build -o ./client ./cmd/client/terragpioclient.go     
 echo "Copying"
-<<<<<<< HEAD
 scp ./server pi@10.15.21.124:/home/pi/bins
 scp ./client pi@10.15.21.124:/home/pi/bins
 echo "Cleaning up"
 rm ./server
 rm ./client
-=======
-scp ./server pi@10.15.21.201:/home/pi/bins
-scp ./client pi@10.15.21.201:/home/pi/bins
->>>>>>> refs/remotes/origin/main
 echo "***********     All Done        ***********"
